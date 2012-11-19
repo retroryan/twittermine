1b54424e8f515227fc13afd69b43d299c30d5f40
Ryan Knight <ryan@grandcloud.com>
2012-11-18 22:10:26 -0700

    updating mongo uri and stopwords

--- app/controllers/Utils.scala
+++ app/controllers/Utils.scala
@@ -5,54 +5,29 @@ import scala.Predef._
 import collection.mutable.ListBuffer
 import models.Tweet
 import play.api.libs.json.Json
-import collection.mutable
 
 
 object Utils {
 
   val wordIndx = loadStopWords()
 
-
-  def loadStopWords() = {
-
-    val fileSource = Source.fromFile("data/stopwords.csv")
-    readOperation(fileSource) {
-         file => {
-           file.getLines().flatMap(_.split(","))
-         }
-    }
-
-
-   /* val src = Source.fromFile("data/stopwords.csv")
-    val stopWords = src.getLines().flatMap(_.split(","))
-
-    //for some reason it seems this collection leaves some kind of pointer to a file descriptor in the collection
-    //so we copy the strings out.  The error when trying to access the collection without this is:
-    //Caused by: java.io.IOException: Bad file descriptor
-
-    val buf = new ListBuffer[String]
-    for (nxt <- stopWords) {
-      buf += nxt
-    }
-
-    src.close()*/
-
-    //val buf = new ListBuffer[String]
-  }
-
   //define a structural type Closable that represents and resource that has a close
-  type Closable = { def close(): Unit }
+  type Closable = {def close(): Unit}
 
 
   /**
    * This excecutes a read file operation on an resource of type Closeable, ensuring at the end that the resource is closed.
    *
+   * For some reason it seems this collection leaves some kind of pointer to a file descriptor in the collection
+   * so we copy the strings out.  The error when trying to access the collection without this is:
+   * Caused by: java.io.IOException: Bad file descriptor
+
    * @param resource
    * @param readOperation
    * @tparam A
    * @return
    */
-  def readOperation[A <: Closable](resource : A)(readOperation: A => Iterator[String]):ListBuffer[String] = {
+  def readOperation[A <: Closable](resource: A)(readOperation: A => Iterator[String]): ListBuffer[String] = {
     try {
       val data = readOperation(resource)
       val buf = new ListBuffer[String]
@@ -71,11 +46,21 @@ object Utils {
    * @tparam A
    * @return
    */
-  def writeOpertaion[A <: Closable](resource : A)(writeOperation: A => Any):Unit = {
+  def writeOpertaion[A <: Closable](resource: A)(writeOperation: A => Any): Unit = {
     try {
       writeOperation(resource)
     } finally {
       resource.close()
+    }
+  }
+
+  def loadStopWords() = {
+
+    val fileSource = Source.fromFile("data/stopwords.csv")
+    readOperation(fileSource) {
+      file => {
+        file.getLines().flatMap(_.split(","))
+      }
     }
   }
 
@@ -90,7 +75,7 @@ object Utils {
    * This is a one time use function to write a list of tweets to file for testing purposes
    * @param tweetList
    */
-  def saveTweetsToFile(tweetList: List[Tweet]):Unit = {
+  def saveTweetsToFile(tweetList: List[Tweet]): Unit = {
 
     import java.io._
 
@@ -104,5 +89,8 @@ object Utils {
       file.print(tweetStrings.mkString("[", ", ", "]"))
     })
   }
+
+
+
 
 }
